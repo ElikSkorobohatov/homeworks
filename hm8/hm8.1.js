635a4249777d29a8a380afc7e56302a2e971f842
Elik Skorobohatov <ee.sd.2005@gmail.com>
2024-05-31 16:14:27 +0300

    hm8

---
+++ hm8/hm8.1.js
@@ -0,0 +1,29 @@
+let ladder = {
+    lader: 0,
+    up: function () {
+        this.lader++;
+        if(this.lader > 13 ){ //В среднем в лестнице 13 ступенек)
+            this.lader= 13;
+        }
+        console.log(this.lader);
+        return this;
+    },
+    down: function () {
+        this.lader--;
+        if(this.lader < 0){
+            this.lader= 0;
+        }
+        console.log(this.lader);
+        return this;
+    },
+    showStep: function () {
+        console.log('You on ' + this.lader + '-th ladders now!');
+        return this;
+    }
+};
+
+ladder.up();
+
+ladder.down();
+
+ladder.showStep();
